a584743fad9ae44036a1b2d9d27a600adf737d28
Amanda McGivern <amanda.marie.mcgivern@gmail.com>
2021-02-08 22:21:26 -0500

    Working on recipe grid. Pulling in data and populating cards

--- client/src/Components/RecipeGrid.js
+++ client/src/Components/RecipeGrid.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Image, Card, Icon } from 'semantic-ui-react'
+import { Grid, Image} from 'semantic-ui-react'
+import RecipeCard from './RecipeCard.js'
 
 class RecipeGrid extends React.Component{
     constructor(props) {
       super(props);
-
+      this.state = { recipes: [], };
+      this.get_recipe_data = this.get_recipe_data.bind(this);
+      this.get_recipe_data();
     }
-    get_recipe_data(id){
+    get_recipe_data(){
         fetch('http://localhost:3001/recipes').then(response => {
             if (response.status !== 200) {
                 console.log('Looks like there was a problem. Status Code: ' +
@@ -17,37 +20,27 @@ class RecipeGrid extends React.Component{
         
               // Examine the text in the response
             response.json().then(data => {
-                console.log(data, response.status);
+                console.log(data.recipes, response.status);
                 this.setState(state => ({
-                    recipes: data,
+                    recipes: data.recipes,
                 }))
             });
         })
     }
 
     render(){
+        let recipeList = [];
+        for (const [index, value] of this.state.recipes.entries()) {
+            recipeList.push(
+                <RecipeCard key={index} props={value}>
+                </RecipeCard>)
+        }
       return (<div>
           <Grid columns='three' divided>
+          {recipeList}
             <Grid.Row>
             <Grid.Column>
-                <Card>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-                    <Card.Content>
-                    <Card.Header>Matthew</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        Matthew is a musician living in Nashville.
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                    <a>
-                        <Icon name='user' />
-                        22 Friends
-                    </a>
-                    </Card.Content>
-                </Card>
+
             </Grid.Column>
             <Grid.Column>
                 <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
