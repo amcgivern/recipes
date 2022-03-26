b2f78004511bcc754d5e577cda4eb8f9ff16e3e1
Amanda McGivern <amanda.marie.mcgivern@gmail.com>
2022-03-26 15:08:04 -0400

    Installed SASS

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Account } from './components/Account.js';
